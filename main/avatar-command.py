1a51490d2e11f5d5cd88798b8ee6ebe6ede24bd6
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-07-30 19:04:16 +0530

    Enhance button interaction response handling
    Added response message and original response deletion after button click.

--- main/avatar-command.py
+++ main/avatar-command.py
@@ -21,6 +21,8 @@ class AButton(discord.ui.View):
     @discord.ui.button(label="Open Browser",style=discord.ButtonStyle.secondary)
     async def call(self,interaction:discord.Interaction,button:discord.ui.Button):
         webbrowser.open(self.link)
+        await interaction.response.send_message("Done!",ephemeral=True)
+        await interaction.delete_original_response()
 
 @bot.command()
 async def av(ctx:commands.Context):
@@ -31,6 +33,7 @@ async def av(ctx:commands.Context):
     await ctx.send(embed=embed,view=button)
 
 bot.run("TOKEN")
+#solved interaction problem! just make sure that interaction comes in the three seconds, don't let defer win!
 #it will work, a button will come and by clicking on it, a new website will open which will be your pfp but it will still show that an error happened, of time out, which makes sense because you are not using an interaction
 #a view or layout view needs interaction, even app commands, slash commands need and if you are not using it, then it wouldn't have defear which gives time to the interaction, and if there's no defer then interaction only has three seconds
 #and there's a lot about it
