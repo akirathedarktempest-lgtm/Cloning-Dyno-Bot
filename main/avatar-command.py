0c059a801c22f63d47bbf795ade8a3a8f15ee38f
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-07-30 18:54:01 +0530

    Add random color generation for avatar embed

--- main/avatar-command.py
+++ main/avatar-command.py
@@ -2,6 +2,13 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import webbrowser
+import random
+
+def colorRandom():
+    hexColor=""
+    for _ in range(6):
+        hexColor+=random.choice("0123456789abcdef")
+    return int(hexColor,16)
 
 intents=discord.Intents.all()
 bot=commands.Bot(command_prefix=commands.when_mentioned_or("?"),intents=intents)
@@ -17,7 +24,7 @@ class AButton(discord.ui.View):
 
 @bot.command()
 async def av(ctx:commands.Context):
-    embed=discord.Embed()
+    embed=discord.Embed(color=colorRandom())
     image=ctx.author.display_avatar.url
     embed.set_image(url=image)
     button=AButton(image)
